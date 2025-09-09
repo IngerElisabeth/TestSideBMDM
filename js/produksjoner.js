483fda4e061134212548c2476b8569a1de46be14
IngerElisabeth <ingerg@afk.no>
2025-09-09 13:20:30 +0200

    Legger inn BMDM filer 9sept

---
+++ js/produksjoner.js
@@ -0,0 +1,44 @@
+
+
+forestillinger =[
+
+{
+    dato: 2026,
+    tittel: "Dalmatinerne",
+    innhold: "Settes opp i Sandvika teater våren 2026",
+    bildemappe: "Dalmatinerne2026/", 
+    bilder: [   
+                {bildefil: "img1.jpg", bildetekst: "", bildekreditt: "Inger Elisabeth"}
+            ]             
+},
+{
+    dato: 2025,
+    tittel: "Grease",
+    innhold: "Ble satt opp på Sandvika teater mars 2025",
+    bildemappe: "Grease2025/",
+    bilder: [
+         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"},
+         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
+
+    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+},
+{
+    dato: 2024,
+    tittel: "Aladdin",
+    innhold: "Ble satt opp på Sandvika teater våren 2024",
+    bildemappe: "Aladdin2024/",
+    bilder: [
+         {bildefil: "Aladdin1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
+    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+},
+{
+    dato: 2023,
+    tittel: "Jungelboken",
+    innhold: "Ble satt opp på Sandvika teater våren 2023",
+    bildemappe: "Jungelboken2023/",
+    bilder: [
+         {bildefil: "Jungelboken1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
+    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+}
+
+];
